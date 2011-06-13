8cb56d8067045b8d1005cd2a2576eb8d402db36e
Nathan Alday <n.c.alday@gmail.com>
2011-06-12 20:24:41 -0500

    Fixed clean_documentation.sh script so it actually cleans up the built documentation.

--- utilities/cleanup_documentation.sh
+++ utilities/cleanup_documentation.sh
@@ -2,6 +2,8 @@
 
 PDFS="01layoutalt 01layout example_controller_layout"
 
+cd ../documents
+
 for FILE in $PDFS; do
     rm $FILE.pdf
 done
@@ -9,3 +11,5 @@ done
 rm *.eps
 rm *.aux
 rm *.log
+rm *.gz
+rm esp.pdf
